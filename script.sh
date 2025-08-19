83192b9a9aa6324c43f6218db1fc3a86bff8c654
Aman6125 <singhaaman006@gmail.com>
2025-08-19 15:58:34 +0530

    count_total_no_of_file_in_folder
    This script will count the total number of files in the folder and if you deleted a file from the folder then will terminate and print again the total number of file

---
+++ script.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+directory_to_check= /d/New folder/countdirfile
+
+while [ "$(ls -A $directory_to_check)" ] #till the time directory exists keep running the loop
+do
+	file_count=$(ls -1 $directory_to_check | wc -l)
+	echo "File count in the direct is $file_count"
+	sleep 5
+done
+
+echo "Directory is now empty"
+
+
